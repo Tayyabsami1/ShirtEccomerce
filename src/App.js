fb7e9d28d7ffceb87f87541c4f22fc4f293e5ef2
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2024-07-22 00:57:52 +0500

    Final changes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import { fileURLToPath } from 'url';
 const App=express();
 
 // Setting the view engine to EJS
-App.set('view engine', 'ejs');
+// App.set('view engine', 'ejs');
 
 // Getting the current directory path for relative paths in views folder
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-App.set('Views', path.join(__dirname,'Views'));
+// App.set('Views', path.join(__dirname,'Views'));
 
 
-App.use('/views',express.static(path.join(__dirname,'/views')));
+// App.use('/views',express.static(path.join(__dirname,'/views')));
 App.use(express.static("public"))
 
 
